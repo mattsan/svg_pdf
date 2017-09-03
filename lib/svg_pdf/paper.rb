1c22ee11358cdc0d19b85c8b073720508c392bbb
Eiji MATSUMOTO <e.mattsan@gmail.com>
2017-09-03 23:08:32 +0900

    add paper size function

--- lib/svg_pdf/paper.rb
+++ lib/svg_pdf/paper.rb
@@ -1,5 +1,61 @@
 module SvgPdf
+  def self.PaperSize(paper_size, orientation = :portrait)
+    PaperSize.new(*PaperSize.const_get(orientation.capitalize)[paper_size.capitalize.to_sym])
+  end
+
   class PaperSize
+    Portrait = {
+      A0: [841, 1189],
+      A1: [594, 841],
+      A2: [420, 594],
+      A3: [297, 420],
+      A4: [210, 297],
+      A5: [148, 210],
+      A6: [105, 148],
+      A7: [74, 105],
+      A8: [52, 74],
+      A9: [37, 52],
+      A10: [26, 37],
+
+      B0: [1000, 1414],
+      B1: [707, 1000],
+      B2: [500, 707],
+      B3: [353, 500],
+      B4: [250, 353],
+      B5: [176, 250],
+      B6: [125, 176],
+      B7: [88, 125],
+      B8: [63, 88],
+      B9: [44, 63],
+      B10: [31, 44]
+    }
+
+    Landscape = {
+      A0: Portrait[:A0].reverse,
+      A1: Portrait[:A1].reverse,
+      A2: Portrait[:A2].reverse,
+      A3: Portrait[:A3].reverse,
+      A4: Portrait[:A4].reverse,
+      A5: Portrait[:A5].reverse,
+      A6: Portrait[:A6].reverse,
+      A7: Portrait[:A7].reverse,
+      A8: Portrait[:A8].reverse,
+      A9: Portrait[:A9].reverse,
+      A10: Portrait[:A10].reverse,
+
+      B0: Portrait[:B0].reverse,
+      B1: Portrait[:B1].reverse,
+      B2: Portrait[:B2].reverse,
+      B3: Portrait[:B3].reverse,
+      B4: Portrait[:B4].reverse,
+      B5: Portrait[:B5].reverse,
+      B6: Portrait[:B6].reverse,
+      B7: Portrait[:B7].reverse,
+      B8: Portrait[:B8].reverse,
+      B9: Portrait[:B9].reverse,
+      B10: Portrait[:B10].reverse
+    }
+
     attr_reader :width, :height
 
     def initialize(width, height)
@@ -26,31 +82,5 @@ module SvgPdf
     def landscape
       PaperSize.new(@height, @width)
     end
-  end
-
-  class PaperSize
-    A0 = PaperSize.new(841, 1189)
-    A1 = PaperSize.new(594, 841)
-    A2 = PaperSize.new(420, 594)
-    A3 = PaperSize.new(297, 420)
-    A4 = PaperSize.new(210, 297)
-    A5 = PaperSize.new(148, 210)
-    A6 = PaperSize.new(105, 148)
-    A7 = PaperSize.new(74, 105)
-    A8 = PaperSize.new(52, 74)
-    A9 = PaperSize.new(37, 52)
-    A10 = PaperSize.new(26, 37)
-
-    B0 = PaperSize.new(1000, 1414)
-    B1 = PaperSize.new(707, 1000)
-    B2 = PaperSize.new(500, 707)
-    B3 = PaperSize.new(353, 500)
-    B4 = PaperSize.new(250, 353)
-    B5 = PaperSize.new(176, 250)
-    B6 = PaperSize.new(125, 176)
-    B7 = PaperSize.new(88, 125)
-    B8 = PaperSize.new(63, 88)
-    B9 = PaperSize.new(44, 63)
-    B10 = PaperSize.new(31, 44)
   end
 end
